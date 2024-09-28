4b68f45fffa1bc0cac20d7a7dcd885768f611ea5
Taariq Pala <taariqpl@amazon.com>
2024-09-28 21:38:46 +0100

    Implemented Expenses Table

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,69 @@
+import { useEffect, useState } from "react";
+
+interface Expense {
+    id: number;
+    merchant: string;
+    amount: number;
+    description: string;
+    date: string;
+    category: string;
+    status: string;
+}
+
 function App() {
-  return (
-    <div id="template-text">
-      <h1>React Starter Template - TypeScript</h1>
-      <p>
-        For JavaScript please use{" "}
-        <a href="https://github.com/ruairidhflint/react-template">this</a>{" "}
-        template
-      </p>
-    </div>
-  );
+    const [expenses, setExpenses] = useState<Expense[]>([]);
+
+    useEffect(() => {
+        const url = "https://expenses-backend-mu.vercel.app/expenses";
+        const fetchData = async () => {
+            try {
+                const response = await fetch(url, {
+                    headers: {
+                        "Content-Type": "application/json",
+                        Username: "taariq.pala",
+                    },
+                });
+                const json = await response.json();
+                setExpenses(json);
+            } catch (err) {
+                console.log(err);
+            }
+        };
+        fetchData();
+    }, []);
+
+    return (
+        <div id="template-text">
+            <h1>Expenses</h1>
+            <hr className="solid"/>
+            <table>
+                <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Merchant</th>
+                    <th>Amount</th>
+                    <th>Description</th>
+                    <th>Date</th>
+                    <th>Category</th>
+                    <th>Status</th>
+                </tr>
+                </thead>
+                <tbody>
+                {expenses.map((expense) => (
+                    <tr key={expense.id}>
+                        <td>{expense.id}</td>
+                        <td>{expense.merchant}</td>
+                        <td>{expense.amount}</td>
+                        <td>{expense.description}</td>
+                        <td>{new Date(expense.date).toLocaleString()}</td>
+                        <td>{expense.category}</td>
+                        <td>{expense.status}</td>
+                    </tr>
+                ))}
+                </tbody>
+            </table>
+        </div>
+    );
 }
 
 export default App;
